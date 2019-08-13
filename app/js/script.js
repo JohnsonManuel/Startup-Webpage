b13a322e962dbd2239ec437a01dfa61a79b70c5e
Johnson <johnsonmanuel321@gmail.com>
2019-08-13 21:52:54 +0530

    sidenav added

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,10 @@
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-var header = document.getElementsByTagName('header');
+let currentScrollPos = window.pageYOffset;
+let header = document.getElementsByTagName('header');
 
   if (currentScrollPos > prevScrollpos) {
-
     header[0].style.transform= "translateY(-100%)";
   } else {
     header[0].style.transform = "translateY(0)";
@@ -13,15 +12,64 @@ var header = document.getElementsByTagName('header');
   prevScrollpos = currentScrollPos;
 }
 
-document.querySelector('.language-selector').addEventListener('click',changeLanguage);
+var sidenav= document.querySelector('.sidenav');
 
-
-function changeLanguage(){
-  document.querySelector('.language-list').classList.toggle('hide');
-}
+document.querySelector('.language-selector').addEventListener('click',()=>{
+  toggleHide(document.querySelector('.language-list'));
+},false);
 
 document.querySelector('.language-list').addEventListener('click',function(e){
-  if(e.taget=="li"){
-    document.querySelector('language-selector').firstElementChild.TextContent=target.TextContent;
+  if(e.target.matches('a')){
+    document.querySelector('.language-selector').firstElementChild.children[0].textContent= e.target.textContent;
   }
 });
+
+document.querySelector('.hamburger').addEventListener('click',function(){
+  sidenav.classList.add('expand');
+  document.addEventListener('click',clickOutSideNav);
+});
+
+
+function clickOutSideNav(e){
+    console.log(e.target.matches('.sidenav ul'));
+    if(!e.target.matches('.sidenav ul') && !e.target.matches('.hamburger')){
+      sidenav.classList.remove('expand');
+      console.log('Added');
+      document.removeEventListener('click',clickOutSideNav);
+    }
+
+}
+function toggleHide(toToggle){
+  toToggle.classList.toggle('hide');
+}
+
+//drag and drop window handler
+addEventsHandler(window,['drop','dragover'],dropHandler);
+
+function addEventsHandler(target,events,handler){
+  for(let i=0;i<events.length;i++)
+  {
+    target.addEventListener(events[i], handler, true);
+  }
+}
+
+ function dropHandler(e){
+   e.preventDefault();
+ }
+
+ //file dropHandler file select handleFileDrop
+document.getElementById('cv-upload').addEventListener('change',handleFileDrop);
+document.querySelector('.file-dropzone').addEventListener('drop', handleFileDrop);
+
+ function handleFileDrop(){
+   if(this.matches('div'))
+   {
+     var dt = event.dataTransfer;
+     var files = dt.files;
+   }
+   else{
+     var files=this.files;
+   }
+  document.getElementById("fileuploaded").textContent=files[0].name;
+
+ }
